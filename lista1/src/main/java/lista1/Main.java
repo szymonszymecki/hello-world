11aa7ad4a2b2aaa5cbfa2482e3a3184a78c0e36e
szymonszymecki <256789@student.pwr.edu.pl>
2020-10-19 14:43:28 +0200

    Zmiana licznika

--- lista1/src/main/java/lista1/Main.java
+++ lista1/src/main/java/lista1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Message message = new Message();
-		for (int i = 0; i < 20; ++i) {
+		for (int i = 0; i < 30; ++i) {
 			message.setMessage(i);
 		}
 	}

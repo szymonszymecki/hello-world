38f5ebbce76da4f75bd4de5b35821255773c2a33
szymonszymecki <256789@student.pwr.edu.pl>
2020-10-19 13:25:59 +0200

    Commit

---
+++ lista1/src/main/java/lista1/DifferentMessage.java
@@ -0,0 +1,12 @@
+package lista1;
+
+public class DifferentMessage {
+	public void setMsg(int i) {
+		if (i < 10) {
+			System.out.println("Nieparzysta < 10");
+		}
+		else {
+			System.out.println("Nieparzysta >= 10");
+		}
+	}
+}
